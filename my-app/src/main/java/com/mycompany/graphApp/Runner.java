3c5954b28962a3f9c05b56d2f574d84297034931
Dashia <das-bobkova@yandex.ru>
2016-12-21 14:05:07 +0300

    Add graph example about SparseMultigraph  form http://www.grotto-networking.com/JUNG/JUNG2-Tutorial.pdf

---
+++ my-app/src/main/java/com/mycompany/graphApp/Runner.java
@@ -0,0 +1,10 @@
+package com.mycompany.graphApp;
+
+/**
+ * Created by Daria Serebryakova on 21.12.2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        GraphBuilder.sparseMultigraphBuild();
+    }
+}
