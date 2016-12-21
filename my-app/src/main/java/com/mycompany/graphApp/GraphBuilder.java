44e1858256fe63f6dd9f392823918dc201b1479e
Dashia <das-bobkova@yandex.ru>
2016-12-21 14:29:26 +0300

    Add my graph

--- my-app/src/main/java/com/mycompany/graphApp/GraphBuilder.java
+++ my-app/src/main/java/com/mycompany/graphApp/GraphBuilder.java
@@ -6,33 +6,29 @@ import edu.uci.ics.jung.graph.util.EdgeType;
 
 /**
  * Created by Daria Serebryakova on 21.12.2016.
- * Example http://www.grotto-networking.com/JUNG/JUNG2-Tutorial.pdf
+ *
  */
 public class GraphBuilder {
     public static void sparseMultigraphBuild() {
-        // Graph<V, E> where V is the type of the vertices
-        // and E is the type of the edges
-        Graph<Integer, String> g = new SparseMultigraph<Integer, String>();
-        // Add some vertices. From above we defined these to be type Integer.
-        g.addVertex((Integer)1);
-        g.addVertex((Integer)2);
-        g.addVertex((Integer)3);
-        // Add some edges. From above we defined these to be of type String
-        // Note that the default is for undirected edges.
-        g.addEdge("Edge-A", 1, 2); // Note that Java 1.5 auto-boxes primitives
-        g.addEdge("Edge-B", 2, 3);
-        // Let's see what we have. Note the nice output from the
-        // SparseMultigraph<V,E> toString() method
-        System.out.println("The graph g = " + g.toString());
-        // Note that we can use the same nodes and edges in two different graphs.
-        Graph<Integer, String> g2 = new SparseMultigraph<Integer, String>();
-        g2.addVertex((Integer)1);
-        g2.addVertex((Integer)2);
-        g2.addVertex((Integer)3);
-        g2.addEdge("Edge-A", 1,3);
-        g2.addEdge("Edge-B", 2,3, EdgeType.DIRECTED);
-        g2.addEdge("Edge-C", 3, 2, EdgeType.DIRECTED);
-        g2.addEdge("Edge-P", 2,3); // A parallel edge
-        System.out.println("The graph g2 = " + g2.toString());
+
+        Graph<Integer, String> graph = new SparseMultigraph<Integer, String>();
+
+        graph.addVertex((Integer)1);
+        graph.addVertex((Integer)2);
+        graph.addVertex((Integer)3);
+        graph.addVertex((Integer)4);
+        graph.addVertex((Integer)5);
+        graph.addVertex((Integer)6);
+        graph.addVertex((Integer)7);
+
+        graph.addEdge("Edge-A", 1, 2, EdgeType.DIRECTED);
+        graph.addEdge("Edge-B", 2, 3, EdgeType.DIRECTED);
+        graph.addEdge("Edge-C", 3, 2, EdgeType.DIRECTED);
+        graph.addEdge("Edge-D", 1, 5, EdgeType.DIRECTED);
+        graph.addEdge("Edge-E", 6, 7, EdgeType.DIRECTED);
+        graph.addEdge("Edge-F", 7, 3, EdgeType.DIRECTED);
+
+        System.out.println("The graph g = " + graph.toString());
+
     }
 }
