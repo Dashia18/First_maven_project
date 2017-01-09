88f633acb3a164298fe97cf605af5bc5ab2a3302
dbobkova <das-bobkova@yandex.ru>
2017-01-09 15:58:50 +0400

    update graph

--- my-app/src/main/java/com/mycompany/graphApp/GraphBuilder.java
+++ my-app/src/main/java/com/mycompany/graphApp/GraphBuilder.java
@@ -39,19 +39,18 @@ class GraphBuilder {
 
         MyEdge edgeA = new MyEdge("Edge-A",1,96);
         MyEdge edgeB = new MyEdge("Edge-B", 3,96);
-        MyEdge edgeC = new MyEdge("Edge-C", 4,96);
+        MyEdge edgeC = new MyEdge("Edge-G",1,96);
         MyEdge edgeD = new MyEdge("Edge-D", 3,192);
         MyEdge edgeE = new MyEdge("Edge-E", 2,96);
         MyEdge edgeF = new MyEdge("Edge-F",1,96);
-        MyEdge edgeG = new MyEdge("Edge-G",1,96);
 
         graph.addEdge(edgeA, v1, v2, EdgeType.DIRECTED);
         graph.addEdge(edgeB, v2, v3, EdgeType.DIRECTED);
-        graph.addEdge(edgeC, v3, v2, EdgeType.DIRECTED);
+        graph.addEdge(edgeC, v3, v4, EdgeType.DIRECTED);
         graph.addEdge(edgeD, v1, v5, EdgeType.DIRECTED);
         graph.addEdge(edgeE, v4, v5, EdgeType.DIRECTED);
         graph.addEdge(edgeF, v2, v5, EdgeType.DIRECTED);
-        graph.addEdge(edgeG, v3, v4, EdgeType.DIRECTED);
+
 
 
 
