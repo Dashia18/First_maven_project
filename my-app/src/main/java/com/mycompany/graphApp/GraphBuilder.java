8d6b8cab02487dea02a3af56cc9f50dd1b5ce0ca
dbobkova <das-bobkova@yandex.ru>
2017-01-09 16:25:45 +0400

    Create  graph and DijkstraShortestPath algorithm.

--- my-app/src/main/java/com/mycompany/graphApp/GraphBuilder.java
+++ my-app/src/main/java/com/mycompany/graphApp/GraphBuilder.java
@@ -3,9 +3,7 @@ package com.mycompany.graphApp;
 import edu.uci.ics.jung.algorithms.flows.EdmondsKarpMaxFlow;
 import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
 import edu.uci.ics.jung.graph.DirectedGraph;
-import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
-import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.graph.SparseMultigraph;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
 
 import org.apache.commons.collections15.Factory;
@@ -23,95 +21,78 @@ import java.util.Map;
 class GraphBuilder {
      static void sparseMultigraphBuild() {
 
-         DirectedGraph<MyVertex, MyEdge> graph = new DirectedSparseMultigraph<MyVertex, MyEdge>();
+         DirectedGraph<MyVertex, MyEdge> graph = new DirectedSparseGraph<MyVertex, MyEdge>();
 
-        MyVertex v1 = new MyVertex(1);
-        MyVertex v2 = new MyVertex(2);
-        MyVertex v3 = new MyVertex(3);
-        MyVertex v4 = new MyVertex(4);
-        MyVertex v5 = new MyVertex(5);
+         MyVertex v1 = new MyVertex(1);
+         MyVertex v2 = new MyVertex(2);
+         MyVertex v3 = new MyVertex(3);
+         MyVertex v4 = new MyVertex(4);
+         MyVertex v5 = new MyVertex(5);
 
-        graph.addVertex(v1);
-        graph.addVertex(v2);
-        graph.addVertex(v3);
-        graph.addVertex(v4);
-        graph.addVertex(v5);
+         graph.addVertex(v1);
+         graph.addVertex(v2);
+         graph.addVertex(v3);
+         graph.addVertex(v4);
+         graph.addVertex(v5);
 
-        MyEdge edgeA = new MyEdge("Edge-A",1,96);
-        MyEdge edgeB = new MyEdge("Edge-B", 3,96);
-        MyEdge edgeC = new MyEdge("Edge-G",1,96);
-        MyEdge edgeD = new MyEdge("Edge-D", 3,192);
-        MyEdge edgeE = new MyEdge("Edge-E", 2,96);
-        MyEdge edgeF = new MyEdge("Edge-F",1,96);
+         MyEdge edgeA = new MyEdge("Edge-A", 1, 96);
+         MyEdge edgeB = new MyEdge("Edge-B", 3, 96);
+         MyEdge edgeC = new MyEdge("Edge-G", 1, 96);
+         MyEdge edgeD = new MyEdge("Edge-D", 3, 192);
+         MyEdge edgeE = new MyEdge("Edge-E", 2, 96);
+         MyEdge edgeF = new MyEdge("Edge-F", 1, 96);
 
-        graph.addEdge(edgeA, v1, v2, EdgeType.DIRECTED);
-        graph.addEdge(edgeB, v2, v3, EdgeType.DIRECTED);
-        graph.addEdge(edgeC, v3, v4, EdgeType.DIRECTED);
-        graph.addEdge(edgeD, v1, v5, EdgeType.DIRECTED);
-        graph.addEdge(edgeE, v4, v5, EdgeType.DIRECTED);
-        graph.addEdge(edgeF, v2, v5, EdgeType.DIRECTED);
+         graph.addEdge(edgeA, v1, v2);
+         graph.addEdge(edgeB, v2, v3);
+         graph.addEdge(edgeC, v3, v4);
+         graph.addEdge(edgeD, v1, v5);
+         graph.addEdge(edgeE, v4, v5);
+         graph.addEdge(edgeF, v2, v5);
 
+         System.out.println("graph: \n" + graph.toString());
 
+         //Minimal path for unweighted graph
+             DijkstraShortestPath<MyVertex, MyEdge> algWeight2 = new DijkstraShortestPath<MyVertex, MyEdge>(graph);
+             List<MyEdge> edgeList2 = algWeight2.getPath(v1, v5);
+             Number dist2 = algWeight2.getDistance(v1, v5);
 
+             System.out.println("The shortest unweighted path from" + v1 + " to " + v5 + " is:");
+             System.out.println(edgeList2.toString());
+             System.out.println("and the length of the path is: " + dist2);
 
+         //Minimal path with for weighted graph
+             Transformer<MyEdge, Double> wtTransformer = new Transformer<MyEdge, Double>() {
+                 public Double transform(MyEdge edge) {
+                     return edge.getWeight();
+                 }
+             };
+             DijkstraShortestPath<MyVertex, MyEdge> algWeight = new DijkstraShortestPath<MyVertex, MyEdge>(graph, wtTransformer);
+             List<MyEdge> edgeList1 = algWeight.getPath(v1, v5);
+             Number dist1 = algWeight.getDistance(v1, v5);
 
-        System.out.println("graph: \n" + graph.toString());
-
-
-        //Minimal path for unweighted graph
-            DijkstraShortestPath<MyVertex,MyEdge> algWeight2 = new DijkstraShortestPath<MyVertex,MyEdge>(graph);
-            List<MyEdge> edgeList2 = algWeight2.getPath(v1,v5);
-            Number dist2 = algWeight2.getDistance(v1, v5);
-
-            System.out.println("The shortest unweighted path from" + v1 +" to " + v5 + " is:");
-            System.out.println(edgeList2.toString());
-            System.out.println("and the length of the path is: " + dist2);
-
-        //Minimal path with for weighted graph
-            Transformer<MyEdge, Double> wtTransformer = new Transformer<MyEdge,Double>() {
-                public Double transform(MyEdge edge) {
-                    return edge.getWeight();
-                }
-            };
-            DijkstraShortestPath<MyVertex,MyEdge> algWeight = new DijkstraShortestPath<MyVertex,MyEdge>(graph, wtTransformer);
-            List<MyEdge> edgeList1 = algWeight.getPath(v1,v5);
-            Number dist1 = algWeight.getDistance(v1, v5);
-
-            System.out.println("\"The shortest path from" + v1 +" to " + v5 + " is:");
-            System.out.println(edgeList1.toString());
-            System.out.println("and the length of the path is: " + dist1);
-
-
-
-
-
+             System.out.println("\"The shortest path from" + v1 + " to " + v5 + " is:");
+             System.out.println(edgeList1.toString());
+             System.out.println("and the length of the path is: " + dist1);
 
          //Max-Flow
-         Transformer<MyEdge, Double> capTransformer =
-                 new Transformer<MyEdge, Double>(){
-                     public Double transform(MyEdge link) {
-                         return link.getCapacity();
-                     }
-                 };
-         Map<MyEdge, Double> edgeFlowMap = new HashMap<MyEdge, Double>();
-         // This Factory produces new edges for use by the algorithm
-         Factory<MyEdge> edgeFactory = new Factory<MyEdge>() {
-             public MyEdge create() {
-                 return new MyEdge(1.0, 1.0);
-             }
-         };
+             Transformer<MyEdge, Double> capTransformer =
+                     new Transformer<MyEdge, Double>() {
+                         public Double transform(MyEdge link) {
+                             return link.getCapacity();
+                         }
+                     };
+             Map<MyEdge, Double> edgeFlowMap = new HashMap<MyEdge, Double>();
+             Factory<MyEdge> edgeFactory = new Factory<MyEdge>() {
+                 public MyEdge create() {
+                     return new MyEdge(1.0, 1.0);
+                 }
+             };
 
-         EdmondsKarpMaxFlow<MyVertex, MyEdge> alg =
-                 new EdmondsKarpMaxFlow(graph, v1,v5, capTransformer,edgeFlowMap,edgeFactory);
+             EdmondsKarpMaxFlow<MyVertex, MyEdge> alg =
+                     new EdmondsKarpMaxFlow(graph, v1, v5, capTransformer, edgeFlowMap, edgeFactory);
 
-         alg.evaluate();
-         System.out.println("The max flow is: " + alg.getMaxFlow());
-         System.out.println(alg.getFlowGraph());
-         System.out.println("The edge set is: " + alg.getMinCutEdges().toString());
-
-
-
-
+             alg.evaluate();
+             System.out.println("The max flow is: " + alg.getMaxFlow());
+             System.out.println("The edge set is: " + alg.getMinCutEdges().toString());
      }
-
 }
